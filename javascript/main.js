8bc5101f07096429339344c6a113a77624b047bc
StanNooijen <Stan.nooijen@gmail.com>
2022-09-14 12:48:47 +0200

    animations added

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,46 @@
+
+
 function scrollDown() {
     window.scrollTo(0, 1200);
   }
   function scrollUp() {
     window.scrollTo(0, 0);
   }
+
+function changeStyle(){
+  document.getElementById("btn").style.fontSize = "10rem";
+  document.getElementById("btn").style.textIndent = "0%";
+  document.getElementById("top1").style.marginBottom = "0rem";
+  document.getElementById("top1").style.transition = "5s";
+  document.getElementById("top2").style.marginBottom = "0rem";
+  document.getElementById("top2").style.transition = "5s";
+  document.getElementById("top3").style.marginBottom = "0rem";
+  document.getElementById("top3").style.transition = "5s";
+  document.getElementById("top4").style.marginBottom = "0rem";
+  document.getElementById("top4").style.transition = "5s";
+  document.getElementById("top5").style.marginBottom = "0rem";
+  document.getElementById("top5").style.transition = "5s";
+}
+
+function changeBack(){
+  document.getElementById("btn").style.fontSize = "0rem";
+  document.getElementById("btn").style.textIndent = "120%";
+  document.getElementById("top1").style.marginBottom = "10rem";
+  document.getElementById("top1").style.transition = "5s";
+  document.getElementById("top2").style.marginBottom = "10rem";
+  document.getElementById("top2").style.transition = "5s";
+  document.getElementById("top3").style.marginBottom = "10rem";
+  document.getElementById("top3").style.transition = "5s";
+  document.getElementById("top4").style.marginBottom = "10rem";
+  document.getElementById("top4").style.transition = "5s";
+  document.getElementById("top5").style.marginBottom = "10rem";
+  document.getElementById("top5").style.transition = "5s";
+}
+
+function loaded(){
+  document.getElementById("top1").style.marginBottom = "10rem";
+  document.getElementById("top2").style.marginBottom = "10rem";
+  document.getElementById("top3").style.marginBottom = "10rem";
+  document.getElementById("top4").style.marginBottom = "10rem";
+  document.getElementById("top5").style.marginBottom = "10rem";
+}
