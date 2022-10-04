416da457b18dcab2a0e65bcbcd42b3e5fbdbfafd
StanNooijen <Stan.nooijen@gmail.com>
2022-10-04 16:45:14 +0200

    agile page finished

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,24 @@
+let panels = document.querySelectorAll(".panel");
+let sections = document.querySelectorAll("section");
+let slides = document.querySelectorAll(".slide");
+
 function scrollDown() {
-    window.scrollTo(0, 1200);
-  }
-  function scrollDown1() {
-    window.scrollTo(0, 2400);
-  }
-  function scrollUp() {
-    window.scrollTo(0, 0);
-
-  }
-
-function changeStyle(){
-  document.getElementById("btn").style.fontSize = "10rem";
-  document.getElementById("btn").style.textIndent = "0%";
+  window.scrollTo(0, 1200);
+  document.getElementById("top1").style.marginBottom = "10rem";
+  document.getElementById("top2").style.marginBottom = "10rem";
+  document.getElementById("top3").style.marginBottom = "10rem";
+  document.getElementById("top4").style.marginBottom = "10rem";
+  document.getElementById("top5").style.marginBottom = "10rem";
+  document.getElementById("top6").style.marginBottom = "10rem";
+  document.getElementById("bottom1").style.marginTop = "10rem";
+  document.getElementById("bottom2").style.marginTop = "10rem";
+  document.getElementById("bottom3").style.marginTop = "10rem";
+  document.getElementById("bottom4").style.marginTop = "10rem";
+  document.getElementById("bottom5").style.marginTop = "10rem";
+  document.getElementById("bottom6").style.marginTop = "10rem";
+}
+function scrollDown1() {
+  window.scrollTo(0, 3600);
   document.getElementById("top1").style.marginBottom = "0rem";
   document.getElementById("top1").style.transition = "5s";
   document.getElementById("top2").style.marginBottom = "0rem";
@@ -37,10 +44,8 @@ function changeStyle(){
   document.getElementById("bottom6").style.marginTop = "0rem";
   document.getElementById("bottom6").style.transition = "5s";
 }
-
-function changeBack(){
-  document.getElementById("btn").style.fontSize = "0rem";
-  document.getElementById("btn").style.textIndent = "120%";
+function scrollUp() {
+  window.scrollTo(0, 0);
   document.getElementById("top1").style.marginBottom = "10rem";
   document.getElementById("top1").style.transition = "5s";
   document.getElementById("top2").style.marginBottom = "10rem";
@@ -67,17 +72,35 @@ function changeBack(){
   document.getElementById("bottom6").style.transition = "5s";
 }
 
-function loaded(){
-  document.getElementById("top1").style.marginBottom = "10rem";
-  document.getElementById("top2").style.marginBottom = "10rem";
-  document.getElementById("top3").style.marginBottom = "10rem";
-  document.getElementById("top4").style.marginBottom = "10rem";
-  document.getElementById("top5").style.marginBottom = "10rem";
-  document.getElementById("top6").style.marginBottom = "10rem";
-  document.getElementById("bottom1").style.marginTop = "10rem";
-  document.getElementById("bottom2").style.marginTop = "10rem";
-  document.getElementById("bottom3").style.marginTop = "10rem";
-  document.getElementById("bottom4").style.marginTop = "10rem";
-  document.getElementById("bottom5").style.marginTop = "10rem";
-  document.getElementById("bottom6").style.marginTop = "10rem";
+
+function loaded() {
+  document.getElementById("top1").style.marginBottom = "0rem";
+  document.getElementById("top2").style.marginBottom = "0rem";
+  document.getElementById("top3").style.marginBottom = "0rem";
+  document.getElementById("top4").style.marginBottom = "0rem";
+  document.getElementById("top5").style.marginBottom = "0rem";
+  document.getElementById("top6").style.marginBottom = "0rem";
+  document.getElementById("bottom1").style.marginTop = "0rem";
+  document.getElementById("bottom2").style.marginTop = "0rem";
+  document.getElementById("bottom3").style.marginTop = "0rem";
+  document.getElementById("bottom4").style.marginTop = "0rem";
+  document.getElementById("bottom5").style.marginTop = "0rem";
+  document.getElementById("bottom6").style.marginTop = "0rem";
+}
+
+panels.forEach((panel) => {
+  addEventListener("click", expand);
+});
+
+slides.forEach((slide) => {
+  addEventListener("click", expand);
+})
+
+function expand(e) {
+  removeClasses();
+  e.target.classList.add("hover");
+}
+
+function removeClasses() {
+  sections.forEach((section) => section.classList.remove("hover"));
 }
