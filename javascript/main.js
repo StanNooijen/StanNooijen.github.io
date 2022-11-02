247ea1bf5f03d89eb0ef42882b4c2ce8614afeb7
StanNooijen <Stan.nooijen@gmail.com>
2022-11-02 19:19:53 +0100

    placement change

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,7 @@
+document.documentElement.scrollTop = 0;
+document.body.scrollTop = 0;
+const leftCards = document.getElementById("leftCards");
+const rightCards = document.getElementById("rightCards");
 let panels = document.querySelectorAll(".panel");
 let sections = document.querySelectorAll("section");
 let slides = document.querySelectorAll(".slide");
@@ -6,15 +10,29 @@ function scrollDown() {
   window.scrollTo(0, 800);
 }
 function scrollDown1() {
-  window.scrollTo(0, 3600);
+  window.scrollTo(0, 1700);
+  leftCards.style.transform = "translateX(0rem)";
+  leftCards.style.opacity = "1";
+  leftCards.style.transition = "3s";
+  rightCards.style.transform = "translateX(0rem)";
+  rightCards.style.opacity = "1";
+  rightCards.style.transition = "3s";
 
 }
 function scrollUp() {
   window.scrollTo(0, 0);
+  leftCards.style.transform = "translateX(-30rem)";
+  leftCards.style.opacity = "0";
+  rightCards.style.transform = "translateX(-30rem)";
+  rightCards.style.opacity = "0";
 }
 
 
 function loaded() {
+  leftCards.style.transform = "translateX(-30rem)";
+  leftCards.style.opacity = "0";
+  rightCards.style.transform = "translateX(-30rem)";
+  rightCards.style.opacity = "0";
 }
 
 panels.forEach((panel) => {
