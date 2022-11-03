75990900f07c381c78036c0889ba4232e8b643df
StanNooijen <Stan.nooijen@gmail.com>
2022-11-03 12:51:35 +0100

    animations added

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,7 @@ document.documentElement.scrollTop = 0;
 document.body.scrollTop = 0;
 const leftCards = document.getElementById("leftCards");
 const rightCards = document.getElementById("rightCards");
+const leftCardsTwo = document.getElementById("leftCardsTwo");
 let panels = document.querySelectorAll(".panel");
 let sections = document.querySelectorAll("section");
 let slides = document.querySelectorAll(".slide");
@@ -14,24 +15,31 @@ function scrollDown1() {
   leftCards.style.transform = "translateX(0rem)";
   leftCards.style.opacity = "1";
   leftCards.style.transition = "3s";
+  leftCardsTwo.style.transform = "translateX(0rem)";
+  leftCardsTwo.style.opacity = "1";
+  leftCardsTwo.style.transition = "5s";
   rightCards.style.transform = "translateX(0rem)";
   rightCards.style.opacity = "1";
-  rightCards.style.transition = "3s";
+  rightCards.style.transition = "4s";
 
 }
 function scrollUp() {
   window.scrollTo(0, 0);
-  leftCards.style.transform = "translateX(-30rem)";
+  leftCards.style.transform = "translateX(-25rem)";
   leftCards.style.opacity = "0";
-  rightCards.style.transform = "translateX(-30rem)";
+  leftCardsTwo.style.transform = "translateX(-75rem)";
+  leftCardsTwo.style.opacity = "0";
+  rightCards.style.transform = "translateX(50rem)";
   rightCards.style.opacity = "0";
 }
 
 
 function loaded() {
-  leftCards.style.transform = "translateX(-30rem)";
+  leftCards.style.transform = "translateX(-25rem)";
   leftCards.style.opacity = "0";
-  rightCards.style.transform = "translateX(-30rem)";
+  leftCardsTwo.style.transform = "translateX(-75rem)";
+  leftCardsTwo.style.opacity = "0";
+  rightCards.style.transform = "translateX(50rem)";
   rightCards.style.opacity = "0";
 }
 
