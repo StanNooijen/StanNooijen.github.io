8e3df9c954a11f30bb8dbf13a658ab23a975ed05
StanNooijen <Stan.nooijen@gmail.com>
2022-11-03 16:54:37 +0100

    animation added and button

--- javascript/main.js
+++ javascript/main.js
@@ -1,46 +1,51 @@
 document.documentElement.scrollTop = 0;
 document.body.scrollTop = 0;
-const leftCards = document.getElementById("leftCards");
-const rightCards = document.getElementById("rightCards");
-const leftCardsTwo = document.getElementById("leftCardsTwo");
+const secAdd = 2;
+const allIds = ['leftCards', 'rightCards', 'leftCardsTwo', 'rightImage', 'leftImage', 'rightImageTwo'];
+const leftIds = ['leftCards', 'leftCardsTwo', 'leftImage'];
+const rightIds = ['rightCards', 'rightImage', 'rightImageTwo'];
 let panels = document.querySelectorAll(".panel");
 let sections = document.querySelectorAll("section");
 let slides = document.querySelectorAll(".slide");
 
 function scrollDown() {
   window.scrollTo(0, 800);
+  
 }
 function scrollDown1() {
   window.scrollTo(0, 1700);
-  leftCards.style.transform = "translateX(0rem)";
-  leftCards.style.opacity = "1";
-  leftCards.style.transition = "3s";
-  leftCardsTwo.style.transform = "translateX(0rem)";
-  leftCardsTwo.style.opacity = "1";
-  leftCardsTwo.style.transition = "5s";
-  rightCards.style.transform = "translateX(0rem)";
-  rightCards.style.opacity = "1";
-  rightCards.style.transition = "4s";
-
+  for (var i = 0; i < allIds.length; i++) {
+    document.getElementById(allIds[i]).style.opacity = "1";
+    document.getElementById(allIds[i]).style.transform = "translateX(0%)";
+  }
+  document.getElementById("leftCards").style.transition = "2s";
+  document.getElementById('rightImage').style.transition = "2s";
+  document.getElementById("leftImage").style.transition = "4s";
+  document.getElementById("rightCards").style.transition = "4s";
+  document.getElementById("leftCardsTwo").style.transition = "6s";
+  document.getElementById("rightImageTwo").style.transition = "6s";
 }
 function scrollUp() {
   window.scrollTo(0, 0);
-  leftCards.style.transform = "translateX(-25rem)";
-  leftCards.style.opacity = "0";
-  leftCardsTwo.style.transform = "translateX(-75rem)";
-  leftCardsTwo.style.opacity = "0";
-  rightCards.style.transform = "translateX(50rem)";
-  rightCards.style.opacity = "0";
+  for (var i = 0; i < leftIds.length; i++) {
+    document.getElementById(leftIds[i]).style.opacity = "0";
+    document.getElementById(leftIds[i]).style.transform = "translateX(-100%)";
+  }
+  for (var i = 0; i < rightIds.length; i++) {
+    document.getElementById(rightIds[i]).style.opacity = "0";
+    document.getElementById(rightIds[i]).style.transform = "translateX(100%)";
+  }
 }
 
-
 function loaded() {
-  leftCards.style.transform = "translateX(-25rem)";
-  leftCards.style.opacity = "0";
-  leftCardsTwo.style.transform = "translateX(-75rem)";
-  leftCardsTwo.style.opacity = "0";
-  rightCards.style.transform = "translateX(50rem)";
-  rightCards.style.opacity = "0";
+  for (var i = 0; i < leftIds.length; i++) {
+    document.getElementById(leftIds[i]).style.opacity = "0";
+    document.getElementById(leftIds[i]).style.transform = "translateX(-100%)";
+  }
+  for (var i = 0; i < rightIds.length; i++) {
+    document.getElementById(rightIds[i]).style.opacity = "0";
+    document.getElementById(rightIds[i]).style.transform = "translateX(100%)";
+  }
 }
 
 panels.forEach((panel) => {
